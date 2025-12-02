dee809bcbffc227152bad6bec45befd2a369c82c
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-02 10:04:39 +0800

    sync to locateor

--- app/models/print/model/task.rb
+++ app/models/print/model/task.rb
@@ -6,6 +6,15 @@ module Print
       attribute :gid, :string
       attribute :aim, :string
       attribute :completed_at, :datetime
+
+      after_save_commit :sync_to_locator, if: :saved_change_to_completed_at?
+    end
+
+    def sync_to_locator
+      model = GlobalID::Locator.locate gid
+      model.print_info ||= {}
+      model.print_info.merge! aim => completed_at
+      model.save
     end
 
   end
