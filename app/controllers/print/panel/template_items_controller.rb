19e33b617492a34bc20be0cc7f3e76c6eb3a0820
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-30 00:11:05 +0800

    fix create item

--- app/controllers/print/panel/template_items_controller.rb
+++ app/controllers/print/panel/template_items_controller.rb
@@ -1,15 +1,15 @@
 module Print
   class Panel::TemplateItemsController < Panel::BaseController
     before_action :set_template
-    before_action :set_new_template_item, only: [:new, :create]
+    before_action :set_create_template_item, only: [:create]
 
     private
     def set_template
       @template = Template.find params[:template_id]
     end
 
-    def set_new_template_item
-      @template_item = @template.template_items.build
+    def set_create_template_item
+      @template_item = @template.template_items.build(template_item_params)
     end
 
     def template_item_params
