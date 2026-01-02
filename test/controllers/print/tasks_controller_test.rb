4b935367b2548642b430c65169b6566d9d68b018
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-03 02:19:18 +0800

    for test

--- test/controllers/print/tasks_controller_test.rb
+++ test/controllers/print/tasks_controller_test.rb
@@ -2,5 +2,10 @@
 module Print
   class TasksControllerTest < ActionDispatch::IntegrationTest
 
+    setup do
+      @printer = MqttPrinter.new(id: '2222')
+      @task = @printer.template_tasks.build()
+    end
+
   end
 end
