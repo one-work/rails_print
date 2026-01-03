212926608277a028f2b1edee10b137a66037d4fa
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-04 02:46:32 +0800

    fix ssues

--- test/controllers/print/tasks_controller_test.rb
+++ test/controllers/print/tasks_controller_test.rb
@@ -3,10 +3,5 @@ require 'print_test_helper'
 module Print
   class TasksControllerTest < ActionDispatch::IntegrationTest
 
-    setup do
-      @model = print_tasks(:one)
-      @params = @task.as_json(only: [:printer_id, :body])
-    end
-
   end
 end
