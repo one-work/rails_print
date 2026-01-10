7037988284c7aea5e34df1f61cc7abb32303ce50
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-10 22:07:36 +0800

    for add mqtt prinetr

--- app/controllers/print/admin/mqtt_printers_controller.rb
+++ app/controllers/print/admin/mqtt_printers_controller.rb
@@ -1,6 +1,7 @@
 module Print
   class Admin::MqttPrintersController < Admin::BaseController
     before_action :set_mqtt_printer, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_mqtt_printer, only: [:new, :create]
 
     def index
       @mqtt_printers = MqttPrinter.default_where(default_params).page(params[:page])
@@ -11,7 +12,6 @@ module Print
     end
 
     def new
-      @mqtt_printer.devices.build
     end
 
     def edit
@@ -29,11 +29,12 @@ module Print
     end
 
     def set_new_mqtt_printer
-      @mqtt_printer = MqttPrinter.new(printer_params)
+      @mqtt_printer = MqttPrinter.new(mqtt_printer_params)
     end
 
     def mqtt_printer_params
       params.fetch(:mqtt_printer, {}).permit(
+        :imei,
         devices_attributes: [:aim]
       )
     end
