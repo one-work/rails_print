d5a9d3f6c9d178b3b19495d8969acf0672fa7cc4
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-08 11:08:17 +0800

    alled curd itmes

--- app/controllers/print/panel/mqtt_users_controller.rb
+++ app/controllers/print/panel/mqtt_users_controller.rb
@@ -1,5 +1,14 @@
 module Print
   class Panel::MqttUsersController < Panel::BaseController
 
+    private
+    def mqtt_user_params
+      params.fetch(:mqtt_user, {}).permit(
+        :username,
+        :password,
+        :ip,
+        :is_superuser
+      )
+    end
   end
 end
