7a16129d6720c3ef553fa91362d5a0feff9dbd20
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-11 17:48:01 +0800

    fix print

--- app/controllers/print/panel/tasks_controller.rb
+++ app/controllers/print/panel/tasks_controller.rb
@@ -1,6 +1,7 @@
 module Print
   class Panel::TasksController < Panel::BaseController
     before_action :set_mqtt_printer
+    before_action :set_task, only: [:show, :edit, :update, :destroy, :resend]
 
     def index
       @tasks = @mqtt_printer.tasks.order(id: :desc).page(params[:page])
@@ -10,9 +11,18 @@ module Print
       @mqtt_printer.tasks.todo.delete_all
     end
 
+    def resend
+      @task.print
+      head :ok
+    end
+
     private
     def set_mqtt_printer
       @mqtt_printer = MqttPrinter.find params[:mqtt_printer_id]
+    end
+
+    def set_task
+      @task = @mqtt_printer.tasks.find params[:id]
     end
 
   end
