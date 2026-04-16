0cadc006d66912925632b69f4cf06f84c28074ec
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-16 21:47:47 +0800

    share

--- app/models/print/model/task/deferred_task.rb
+++ app/models/print/model/task/deferred_task.rb
@@ -2,11 +2,5 @@ module Print
   module Model::Task::DeferredTask
     extend ActiveSupport::Concern
 
-    def print
-      if mqtt_printer
-        mqtt_printer.print_cmd(raw_arr, id)
-      end
-    end
-
   end
 end
