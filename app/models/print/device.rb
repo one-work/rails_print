05e55d749863ff604173e24113204fd5f92b7551
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-20 20:25:59 +0800

    should remove device

--- app/models/print/device.rb
+++
@@ -1,5 +0,0 @@
-module Print
-  class Device < ApplicationRecord
-    include Model::Device
-  end
-end
--- app/models/print/model/device.rb
+++ app/models/print/model/device.rb
@@ -12,14 +12,10 @@ module Print
       }, prefix: true
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
-      belongs_to :printer
 
-      before_validation :sync_organ_from_printer, if: :new_record?
     end
 
-    def sync_organ_from_printer
-      self.organ_id = printer.organ_id
-    end
+   
 
     def print(gid, &block)
       task = printer.inner_tasks.build(gid: gid, aim: aim)
