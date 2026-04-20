823d9e2ebb76016dc02c840f1053dfbe8af5f357
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-20 19:58:39 +0800

    reduce code

--- app/models/print/model/printer/bluetooth_printer.rb
+++ app/models/print/model/printer/bluetooth_printer.rb
@@ -22,11 +22,7 @@ module Print
       has_many :devices, as: :printer, dependent: :delete_all
       accepts_nested_attributes_for :devices, allow_destroy: true
 
-      has_many :tasks, as: :printer, dependent: :delete_all
-      has_many :template_tasks, as: :printer, dependent: :delete_all
-      has_many :raw_tasks, as: :printer, dependent: :delete_all
-      has_many :deferred_tasks, as: :printer, dependent: :delete_all
-      has_many :inner_tasks, as: :printer, dependent: :delete_all
+
 
       validates :name, uniqueness: { scope: :organ_id }
     end
