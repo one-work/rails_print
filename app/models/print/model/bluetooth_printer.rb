5ed7939ff23cb0e1875cbb66f2d4977036ef6293
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-20 18:30:09 +0800

    simple codes

--- app/models/print/model/bluetooth_printer.rb
+++ app/models/print/model/bluetooth_printer.rb
@@ -4,6 +4,18 @@ module Print
 
     included do
       attribute :name, :string
+      attribute :dev_type, :integer
+      attribute :dev_cut_type, :integer
+
+      enum :dev_type, {
+        cpcl: 1,
+        esc: 2
+      }, default: 'cpcl', prefix: true
+
+      enum :dev_cut_type, {
+        full: 0,
+        partial: 1
+      }, default: 'full', prefix: true
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
--- app/models/print/model/device.rb
+++ app/models/print/model/device.rb
@@ -14,8 +14,6 @@ module Print
       belongs_to :organ, class_name: 'Org::Organ', optional: true
       belongs_to :printer, polymorphic: true
 
-      has_many :inner_tasks
-
       before_validation :sync_organ_from_printer, if: :new_record?
     end
 
@@ -24,7 +22,7 @@ module Print
     end
 
     def print(gid, &block)
-      task = inner_tasks.build(gid: gid, aim: aim)
+      task = printer.inner_tasks.build(gid: gid, aim: aim)
       task.set_esc!(&block)
     end
 
