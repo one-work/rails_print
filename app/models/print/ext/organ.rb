2425d3fddc366a0d95f964b4862849728d5f942a
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-13 15:50:05 +0800

    fix

--- app/models/print/ext/organ.rb
+++ app/models/print/ext/organ.rb
@@ -2,6 +2,13 @@
 
 module Print
   module Ext::Organ
+    extend ActiveSupport::Concern
+
+    included do
+      attribute :printer_aims_count, :integer, default: 0
+
+      has_many :printer_aims, class_name: 'Print::PrinterAim'
+    end
 
     def get_printer(aim)
       printer_aims = PrinterAim.includes(:printer).where(printer: { online: true }, aim: aim, organ_id: self.id)
--- app/models/print/model/printer_aim.rb
+++ app/models/print/model/printer_aim.rb
@@ -5,7 +5,7 @@ module Print
     included do
       attribute :aim, :string
 
-      belongs_to :organ, class_name: 'Org::Organ', optional: true
+      belongs_to :organ, class_name: 'Org::Organ', counter_cache: true, optional: true
       belongs_to :printer
 
       has_many :inner_tasks, primary_key: [:printer_id, :aim], foreign_key: [:printer_id, :aim]
